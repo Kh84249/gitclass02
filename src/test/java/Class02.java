9c68a1f5ae23590e7e74974a486499b4154dcf91
Kh84249 <kshoaiby@gmail.com>
2026-03-25 09:50:59 +0100

    second commit

--- src/test/java/Class02.java
+++ src/test/java/Class02.java
@@ -4,6 +4,10 @@ public class Class02 {
 
 
         System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("this is the project version 1");
+
+        System.out.println("this is the project version 2");
+        System.out.println("this is the project version 3");
+        System.out.println("this is the project version 4");
     }
 }
