6b3ee87d04e8c3f5f63901830da56076f84256fd
Kh84249 <kshoaiby@gmail.com>
2026-03-25 09:41:41 +0100

    first commit

---
+++ src/test/java/Class02.java
@@ -0,0 +1,9 @@
+public class Class02 {
+
+    static void main() {
+
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
--- src/test/java/class01.java
+++ src/test/java/class01.java
@@ -3,6 +3,6 @@ public class class01 {
     static void main() {
 
         System.out.println("i am first project");
-        
+
     }
 }
